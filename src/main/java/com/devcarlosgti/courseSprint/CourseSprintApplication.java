eed234cfe7010bbcdd4c2e0ae26454b47608a2e1
Carlos-GleysonTI <c.gleysonti@gmail.com>
2024-05-02 10:17:37 -0300

    Project created

---
+++ src/main/java/com/devcarlosgti/courseSprint/CourseSprintApplication.java
@@ -0,0 +1,13 @@
+package com.devcarlosgti.courseSprint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseSprintApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseSprintApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devcarlosgti/courseSprint/CourseSprintApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devcarlosgti.courseSprint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseSprintApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
